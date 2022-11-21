55ecefeb984ec7666e22c846aa28862204b89787
vasil <vmirrchev@gmail.com>
2022-11-21 21:34:24 +0200

    Adding CrossOrigin to client in EntryController

--- sodiumstock.api/src/main/java/com/sodiumstock/controller/EntryController.java
+++ sodiumstock.api/src/main/java/com/sodiumstock/controller/EntryController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials="true")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api")

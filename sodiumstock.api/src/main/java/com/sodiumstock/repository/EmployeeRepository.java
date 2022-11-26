e85fb66fc80dee0f2dae7e591e62ba79a2eaee4e
vasil <vmirrchev@gmail.com>
2022-11-26 22:41:07 +0200

    Adding existsByUsername method to  employee repository fixing mistake

--- sodiumstock.api/src/main/java/com/sodiumstock/repository/EmployeeRepository.java
+++ sodiumstock.api/src/main/java/com/sodiumstock/repository/EmployeeRepository.java
@@ -14,4 +14,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByPhoneNumber(String phoneNumber);
+
+    boolean existsByUsername(String admin);
 }

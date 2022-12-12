dbbe51e0159daf06aef3bba7a4b9367dad3b60e6
vasil <vmirrchev@gmail.com>
2022-12-12 20:52:15 +0200

    Changing import for loginRequest in login.component.ts

--- sodiumstock.client/src/app/login/login.component.ts
+++ sodiumstock.client/src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { LoginResponse } from '../loginResponse';
+import { LoginResponse } from '../login';
 
 @Component({
   selector: 'app-login',
